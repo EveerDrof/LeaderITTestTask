2df46de4dae0c92cc59beb13c665ae4b4840603c
Eveerdrof <hyper00border@gmail.com>
2023-03-06 18:12:39 +0300

    First commit

---
+++ src/main/java/com/example/LeaderITTestTask/LeaderItTestTaskApplication.java
@@ -0,0 +1,13 @@
+package com.example.LeaderITTestTask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeaderItTestTaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LeaderItTestTaskApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/LeaderITTestTask/LeaderItTestTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.LeaderITTestTask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LeaderItTestTaskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
